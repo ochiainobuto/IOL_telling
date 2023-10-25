e51904763038ebb5bac534547a9700d8acaddf60
ochiainobuto <nob55@nifty.com>
2023-10-25 15:24:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,9 @@ if uploaded_file:
 
         with st.chat_message("assistant"): # アシスタントの応答を表示するためのブロックを開始する
             response = pandas_ai.run(new_df, prompt=e_prompt)
-            # jp_response = translate_to_japanese(response)
+            plt.savefig('bar_plot.png')
+            st.image('bar_plot.png')
+            jp_response = translate_to_japanese(response)
             st.write(response) # 応答をStreamlitのチャットに表示する
 
         st.session_state.messages.append({"role": "assistant", "content": response})
